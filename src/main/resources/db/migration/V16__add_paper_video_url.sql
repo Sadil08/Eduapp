95def155ea7e7d95f808404b71672d8b768b271e
sadil <sadil@vizuamatix.com>
2026-02-12 12:04:10 +0530

    Db data

--- src/main/resources/db/migration/V16__add_paper_video_url.sql
+++
@@ -1,2 +0,0 @@
--- Add video_url column to papers table for YouTube explanation videos
-ALTER TABLE papers ADD COLUMN video_url VARCHAR(500);
--- src/main/resources/db/migration/V7__fix_country_column_size.sql
+++
@@ -1,5 +0,0 @@
--- Fix country column size to accommodate full country names from geo-location service
--- The ip-api.com service returns full country names, not ISO codes
-
-ALTER TABLE users
-ALTER COLUMN country TYPE VARCHAR(100);
